a2e79c660a5000b7b821a0f46b77ec6558705c99
Erik Martin-Dorel <erik.martin-dorel@irit.fr>
2020-03-10 17:11:38 +0100

    Make the bash script more robust
    Now OK with:
    $ shellcheck generate.sh

--- generate.sh
+++ generate.sh
@@ -1,7 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
-for f in `dirname $0`/{,.}*.mustache
-do
-    echo "Generating `basename $f .mustache`..."
-    mustache meta.yml $f > `basename $f .mustache`
+srcdir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )
+
+for f in "$srcdir"/{,.}*.mustache; do
+    echo "Generating $(basename "$f" .mustache)..."
+    pystache meta.yml "$f" > "$(basename "$f" .mustache)"
 done
