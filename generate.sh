1bc8d19701803dd3d6322cd27dc83a0c927b554d
Karl Palmskog <palmskog@gmail.com>
2020-03-10 13:10:54 -0500

    change pystache to mustache in generate.sh

--- generate.sh
+++ generate.sh
@@ -4,5 +4,5 @@ srcdir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )
 
 for f in "$srcdir"/{,.}*.mustache; do
     echo "Generating $(basename "$f" .mustache)..."
-    pystache meta.yml "$f" > "$(basename "$f" .mustache)"
+    mustache meta.yml "$f" > "$(basename "$f" .mustache)"
 done
