4de4d6eceab9121afdff39cdc1ce98dfc332f3ac
Erik Martin-Dorel <erik.martin-dorel@irit.fr>
2020-04-26 23:02:01 +0200

    fix: Generate coq-proj.opam instead of coq.opam

--- generate.sh
+++ generate.sh
@@ -3,6 +3,13 @@
 srcdir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )
 
 for f in "$srcdir"/{,.}*.mustache; do
-    echo "Generating $(basename "$f" .mustache)..."
-    mustache meta.yml "$f" > "$(basename "$f" .mustache)"
+    target=$(basename "$f" .mustache)
+    case "$target" in
+        *.opam)
+            shortname=$(grep -e "shortname:" meta.yml | sed -e "s/^shortname: //")
+            target=${target/coq/coq-$shortname}
+            ;;
+    esac
+    echo "Generating $target..."
+    mustache meta.yml "$f" > "$target"
 done
