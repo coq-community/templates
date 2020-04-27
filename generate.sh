acbd3faec00f529d3e22a836fe665e3645df42ea
Erik Martin-Dorel <erik.martin-dorel@irit.fr>
2020-04-27 13:28:51 +0200

    fix: Address review comments by @Zimmi48

--- generate.sh
+++ generate.sh
@@ -7,9 +7,13 @@ function ask1() { local yn; echo -n "$* [Y/n] "; read -r -n 1 yn; [[ "$yn" =~ [Y
 for f in "$srcdir"/{,.}*.mustache; do
     target=$(basename "$f" .mustache)
     case "$target" in
-        *.opam)
-            shortname=$(grep -e "shortname:" meta.yml | sed -e "s/^shortname: //")
-            target=${target/coq/coq-$shortname}
+        coq.opam)
+            shortname=$(grep -e "^shortname:" meta.yml | sed -e 's/^shortname:\s\+//')
+            [ -n "$shortname" ] && target=${target/coq/coq-$shortname}
+            ;;
+        extracted.opam)
+            extracted_shortname=$(grep -e "^\s\+extracted_shortname:" meta.yml | sed -e 's/^\s\+extracted_shortname:\s\+//')
+            [ -n "$extracted_shortname" ] && target=${target/extracted/$extracted_shortname}
             ;;
         coq-action.yml)
             ask1 "Do you want to create a .github/workflows subfolder for coq-action.yml?" &&
